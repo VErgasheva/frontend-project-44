b6ee9ca921d82f314726a6e1105493234469d8c7
VErgasheva <vikser587@gmail.ru>
2024-12-10 17:39:49 +0500

    12

--- src/progress.js
+++ src/progress.js
@@ -1,8 +1,5 @@
 function getprogres(start, step, length) {
     const progression = [];
-    const start = 2; 
-    const step = 3;
-    const length = 5;
     for (let i = 0; i < length; i += 1) {
       progression.push(start + i * step);
     }
