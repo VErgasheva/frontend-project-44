7fddb2298b4df23401947a74f12e59ebba430d4c
VErgasheva <vikser587@gmail.ru>
2024-12-11 15:13:29 +0500

    12

--- src/MindGames/brain-progression.js
+++ src/MindGames/brain-progression.js
@@ -6,7 +6,7 @@ const rules = 'What number is missing in the progression?';
 
 const generateTask = () => {
   const progressionLength = getrandom;
-  const progression = getprogres(getrandom, getrandom, getrandom);
+  const progression = getprogres(2, 3, 5);
   const hiddenIndex = Math.floor(Math.random() * progressionLength);
   const correctAnswer = String(progression[hiddenIndex]);
   progression[hiddenIndex] = '..';
